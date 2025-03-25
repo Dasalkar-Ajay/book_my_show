02ca842013d50f10dc54fc91311537efe3c9a1a2
Dasalkar Ajay <162880775+Dasalkar-Ajay@users.noreply.github.com>
2025-03-25 18:02:06 +0000

    Day 1

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# book_my_show
+book_my_showclone 
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Routes,Route} from "react-router-dom";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <div> Ajju dada jindabad</div>
+  );
+}
+
+export default App;
